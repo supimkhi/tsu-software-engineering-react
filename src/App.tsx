0b6b9e0f1e1641cbbfb8413d8515486d97be148e
Chris Jeff <chrisjeff429@gmail.com>
2022-11-17 16:30:01 -0600

    adds api models and serializers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import About from "./Views/About";
 import Home from "./Views/Home";
+import Search from "./Views/Search"
 
 import React from "react";
 import {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/">
             <Route index element={<Home/>}></Route>
             <Route path="about" element={<About/>}></Route>
+            <Route path="search" element={<Search/>}></Route>
           </Route>
         </Routes>
     </Router>
---
+++ src/Views/Search.tsx
@@ -0,0 +1,12 @@
+import "../App.css";
+
+
+function Search() {
+    return (
+      <div>
+        <h2>Search</h2>
+      </div>
+    );
+}
+
+export default Search;
